c7704ab7de4781dbdfa93b339e730d3231ac7308
zhangjianhui <zhangjianhui@bluemoon.com.cn>
2019-09-28 17:52:31 +0800

    在版本回退后，再做一次提交

--- multibolg/src/test/java/com/zhangshi/multiblog/MultibolgApplicationTests.java
+++ multibolg/src/test/java/com/zhangshi/multiblog/MultibolgApplicationTests.java
@@ -49,5 +49,6 @@ public class MultibolgApplicationTests {
     public void myTestVersionRotation(){
         System.out.println("张大哥测试版本退回——更改测试版版本回退");
         String vesion_01 = "测试版本回退01";
+        String  vesion_03 = "再版本回退后再做一次提交";
     }
 }

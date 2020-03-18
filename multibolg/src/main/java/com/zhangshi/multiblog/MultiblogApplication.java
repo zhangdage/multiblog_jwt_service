39a8d95a2861bbb03d48c87c54c9283e5f4fdbbb
zhangjianhui <1003597986@qq.com>
2020-03-18 09:15:59 +0800

    test

--- multibolg/src/main/java/com/zhangshi/multiblog/MultiblogApplication.java
+++ multibolg/src/main/java/com/zhangshi/multiblog/MultiblogApplication.java
@@ -3,9 +3,11 @@ package com.zhangshi.multiblog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.zhangshi.multiblog.mapper")
+@EnableAspectJAutoProxy
 public class MultiblogApplication {
 
     public static void main(String[] args) {
---
+++ multibolg/src/main/java/com/zhangshi/multiblog/common/aspect/AspectDemo.java
@@ -0,0 +1,9 @@
+package com.zhangshi.multiblog.common.aspect;
+
+/**
+ * @author zhangdage
+ * @create 2020/3/9 1:46
+ */
+@Aspect
+public class AspectDemo {
+}
